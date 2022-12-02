5519fbbb7fea50091f092f044fe7b710cf1d28fe
Kresten Elleby <elleby@kresten.dk>
2022-12-02 22:50:42 +0100

    Day 2 first puzzle

---
+++ AdventOfCode/Day02/Day02.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Day02;
+
+public class Day02
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day02(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    private static string DataPath => Path.Combine(Directory.GetCurrentDirectory(), "Day02\\input.txt");
+
+    [Fact]
+    public async Task Day02SolutionTest()
+    {
+        using var sr = File.OpenText(DataPath);
+        string? lineData;
+        var gameRounds = new List<GameRound>();
+
+        while ((lineData = await sr.ReadLineAsync()) != null)
+        {
+            var tempStrategy = new GameRound(lineData);
+            gameRounds.Add(tempStrategy);
+            //_testOutputHelper.WriteLine(tempStrategy.ToString());
+        }
+        
+        _testOutputHelper.WriteLine($"Total score: {gameRounds.Sum(c=>c.Score)}");
+    }
+
+    [Fact]
+    public void Day02HintTest()
+    {
+        var round1 = new GameRound("A Y");
+        var round2 = new GameRound("B X");
+        var round3 = new GameRound("C Z");
+
+        round1.Opponent.Should().Be(GameRound.WeaponSelection.Rock);
+        round1.Player.Should().Be(GameRound.WeaponSelection.Paper);
+        round1.GetWinner.Should().Be(GameRound.Winner.Player);
+        round1.Score.Should().Be(8);
+        
+        round2.Opponent.Should().Be(GameRound.WeaponSelection.Paper);
+        round2.Player.Should().Be(GameRound.WeaponSelection.Rock);
+        round2.GetWinner.Should().Be(GameRound.Winner.Opponent);
+        round2.Score.Should().Be(1);
+        
+        round3.Opponent.Should().Be(GameRound.WeaponSelection.Scissors);
+        round3.Player.Should().Be(GameRound.WeaponSelection.Scissors);
+        round3.GetWinner.Should().Be(GameRound.Winner.Draw);
+        round3.Score.Should().Be(6);
+
+        var totalScore = round1.Score + round2.Score + round3.Score;
+        totalScore.Should().Be(15);
+    }
+}
---
+++ AdventOfCode/Day02/GameRound.cs
@@ -0,0 +1,102 @@
+﻿namespace AdventOfCode.Day02;
+
+public class GameRound
+{
+    public enum WeaponSelection
+    {
+        Rock,
+        Paper,
+        Scissors,
+        Unknown
+    }
+    
+    public enum Winner
+    {
+        Opponent,
+        Player,
+        Draw
+    }
+
+    public GameRound(string lineData)
+    {
+        Input = lineData;
+        Opponent = ParseStrategy(lineData[0]);
+        Player = ParseStrategy(lineData[2]);
+    }
+
+    private string Input { get; }
+    internal WeaponSelection Opponent { get; }
+    public WeaponSelection Player { get; }
+
+    public int Score
+    {
+        get
+        {
+            var tempScore = 0;
+            switch (Player)
+            {
+                case WeaponSelection.Rock : tempScore = 1;
+                    break;
+                case WeaponSelection.Paper : tempScore = 2;
+                    break;
+                case WeaponSelection.Scissors : tempScore = 3;
+                    break;
+            }
+
+            if (GetWinner == Winner.Player)
+            {
+                tempScore += 6;
+            }
+            if (GetWinner == Winner.Draw)
+            {
+                tempScore += 3;
+            }
+
+            return tempScore;
+        }
+    }
+
+    public Winner GetWinner
+    {
+        get
+        {
+            if (Player == WeaponSelection.Rock && Opponent == WeaponSelection.Scissors)
+            {
+                return Winner.Player;
+            }
+            if (Player == WeaponSelection.Scissors && Opponent == WeaponSelection.Paper)
+            {
+                return Winner.Player;
+            }
+            if (Player == WeaponSelection.Paper && Opponent == WeaponSelection.Rock)
+            {
+                return Winner.Player;
+            }
+            if (Player == Opponent)
+            {
+                return Winner.Draw;
+            }
+
+            return Winner.Opponent;
+        }
+    }
+
+    private WeaponSelection ParseStrategy(char strategyCode)
+    {
+        return strategyCode switch
+        {
+            'A' => WeaponSelection.Rock,
+            'B' => WeaponSelection.Paper,
+            'C' => WeaponSelection.Scissors,
+            'X' => WeaponSelection.Rock,
+            'Y' => WeaponSelection.Paper,
+            'Z' => WeaponSelection.Scissors,
+            _ => WeaponSelection.Unknown
+        };
+    }
+
+    public override string ToString()
+    {
+        return $"Input: {Input} Opponent: {Opponent} Player: {Player} Who won {GetWinner} Score: {Score}";
+    }
+}
