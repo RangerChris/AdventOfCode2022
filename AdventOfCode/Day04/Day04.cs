fc14a949b177da9d2e267548844a43dde44650bc
Kresten Elleby <elleby@kresten.dk>
2022-12-04 10:47:18 +0100

    Puzzle 1, day 4

---
+++ AdventOfCode/Day04/Day04.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+
+namespace AdventOfCode.Day04;
+
+public class Day04
+{
+    private static string DataPath => Path.Combine(Directory.GetCurrentDirectory(), "Day04\\Input.txt");
+    
+    [Fact]
+    public async Task Day04Puzzle1Test()
+    {
+        using var sr = File.OpenText(DataPath);
+        string? lineData;
+        var assignments = new List<SectionAssignment>();
+
+        while ((lineData = await sr.ReadLineAsync()) != null)
+        {
+            assignments.Add(new SectionAssignment(lineData));
+        }
+
+        assignments.Should().HaveCount(1000);
+        var fullyContainsList = assignments.Where(c => c.ContainsFully()).ToList();
+        var notFullyContainsList = assignments.Where(c => c.ContainsFully() == false).ToList();
+        fullyContainsList.Should().HaveCount(466);
+    }
+
+    [Fact]
+    public void Day04Puzzle1HintTest()
+    {
+        var assignment1 = new SectionAssignment("2-4,6-8");
+
+        assignment1.PairList[0].Min.Should().Be(2);
+        assignment1.PairList[0].Max.Should().Be(4);
+        assignment1.PairList[1].Min.Should().Be(6);
+        assignment1.PairList[1].Max.Should().Be(8);
+        
+        var assignment2 = new SectionAssignment("2-8,3-7");
+        var assignment3 = new SectionAssignment("6-6,4-6");
+
+        assignment1.ContainsFully().Should().BeFalse();
+        assignment2.ContainsFully().Should().BeTrue();
+        assignment3.ContainsFully().Should().BeTrue();
+        
+        var assignment4 = new SectionAssignment("2-4,2-5");
+        assignment4.ContainsFully().Should().BeTrue();
+        var assignment5 = new SectionAssignment("2-5,2-4");
+        assignment5.ContainsFully().Should().BeTrue();
+    }
+}
---
+++ AdventOfCode/Day04/SectionAssignment.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode.Day04;
+
+public class SectionAssignment
+{
+    public List<Pair> PairList = new();
+
+    public SectionAssignment(string assignment)
+    {
+        ParsePairInput(assignment);
+    }
+
+    private void ParsePairInput(string pair)
+    {
+        var pairs = pair.Split(",");
+        foreach (var currentPair in pairs)
+        {
+            var input = currentPair.Split("-");
+            var newPair = new Pair(Convert.ToInt32(input[0]), Convert.ToInt32(input[1]));
+            PairList.Add(newPair);
+        }
+    }
+
+    public bool ContainsFully()
+    {
+        var orderedPairs = PairList.OrderBy(c => c.Min).ToList();
+        if (orderedPairs[0].Min <= orderedPairs[1].Min && 
+            orderedPairs[0].Max >= orderedPairs[1].Max)
+        {
+            return true;
+        }
+        if (orderedPairs[1].Min <= orderedPairs[0].Min && 
+            orderedPairs[1].Max >= orderedPairs[0].Max)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override string ToString()
+    {
+        var orderedPairs = PairList.OrderBy(c => c.Min).ToList();
+        return $"{orderedPairs[0].Min}-{orderedPairs[0].Max} {orderedPairs[1].Min}-{orderedPairs[1].Max}. FullyContains: {ContainsFully()}";
+    }
+}
+
+public class Pair
+{
+    public Pair(int min, int max)
+    {
+        Min = min;
+        Max = max;
+    }
+
+    public int Min { get; set; }
+    public int Max { get; set; }
+}
