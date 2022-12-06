a3018379c6c9c9f5d67abae4b08f1f5d09a8e374
Kresten Elleby <elleby@kresten.dk>
2022-12-06 07:08:36 +0100

    Solved puzzle 2, day 6

--- AdventOfCode/Day06/Day06.cs
+++ AdventOfCode/Day06/Day06.cs
@@ -9,23 +9,23 @@ public class Day06
     [Fact]
     public void Day06Puzzle1HintTest()
     {
-        var signal1 = new Signal("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        var signal1 = new Signal("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4);
         var startPosition = signal1.FindStart();
         startPosition.Should().Be(7);
         
-        var signal2 = new Signal("bvwbjplbgvbhsrlpgdmjqwftvncz");
+        var signal2 = new Signal("bvwbjplbgvbhsrlpgdmjqwftvncz", 4);
         startPosition = signal2.FindStart();
         startPosition.Should().Be(5);
         
-        var signal3 = new Signal("nppdvjthqldpwncqszvftbrmjlhg");
+        var signal3 = new Signal("nppdvjthqldpwncqszvftbrmjlhg", 4);
         startPosition = signal3.FindStart();
         startPosition.Should().Be(6);
         
-        var signal4 = new Signal("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+        var signal4 = new Signal("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4);
         startPosition = signal4.FindStart();
         startPosition.Should().Be(10);
         
-        var signal5 = new Signal("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+        var signal5 = new Signal("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4);
         startPosition = signal5.FindStart();
         startPosition.Should().Be(11);
     }
@@ -34,8 +34,29 @@ public class Day06
     public async Task Day06Puzzle1Test()
     {
         var signal = await File.ReadAllTextAsync(DataPath);
-        var signal1 = new Signal(signal);
+        var signal1 = new Signal(signal, 4);
         var startPosition = signal1.FindStart();
         startPosition.Should().Be(1109);
+    }
+
+    [Fact]
+    public void Day06Puzzle2HintTest()
+    {
+        var signal1 = new Signal("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14);
+        var startPosition = signal1.FindStart();
+        startPosition.Should().Be(19);
+        
+        var signal2 = new Signal("bvwbjplbgvbhsrlpgdmjqwftvncz", 14);
+        startPosition = signal2.FindStart();
+        startPosition.Should().Be(23);
+    }
+    
+    [Fact]
+    public async Task Day06Puzzle2Test()
+    {
+        var signal = await File.ReadAllTextAsync(DataPath);
+        var signal1 = new Signal(signal, 14);
+        var startPosition = signal1.FindStart();
+        startPosition.Should().Be(3965);
     }
 }
--- AdventOfCode/Day06/Signal.cs
+++ AdventOfCode/Day06/Signal.cs
@@ -2,10 +2,13 @@
 
 public class Signal
 {
-    public Signal(string signalData)
+    public Signal(string signalData, int signalLength)
     {
         SignalData = signalData;
+        SignalLength = signalLength;
     }
+
+    public int SignalLength { get; set; }
 
     public string SignalData { get; set; }
 
@@ -14,10 +17,10 @@ public class Signal
         var startPosition = 0;
         while (startPosition < SignalData.Length)
         {
-            var content = SignalData.Substring(startPosition, 4);
+            var content = SignalData.Substring(startPosition, SignalLength);
             if (IsAllDifferent(content))
             {
-                return startPosition + 4;
+                return startPosition + SignalLength;
             }
             startPosition++;
         }
@@ -27,24 +30,24 @@ public class Signal
 
     private bool IsAllDifferent(string content)
     {
-        if (content.Length != 4) return false;
+        if (content.Length != SignalLength) return false;
 
         var indexes = new Dictionary<int, int>();
-        var letter1 = content.AllIndexOf(content[0].ToString(), StringComparison.Ordinal);
-        foreach (var currentLetter in letter1) indexes.Add(currentLetter, 0);
 
-        try
+        for (int i = 0; i < SignalLength; i++)
         {
-            var letter2 = content.AllIndexOf(content[1].ToString(), StringComparison.Ordinal);
-            foreach (var currentLetter in letter2) indexes.Add(currentLetter, 0);
-            var letter3 = content.AllIndexOf(content[2].ToString(), StringComparison.Ordinal);
-            foreach (var currentLetter in letter3) indexes.Add(currentLetter, 0);
-            var letter4 = content.AllIndexOf(content[3].ToString(), StringComparison.Ordinal);
-            foreach (var currentLetter in letter4) indexes.Add(currentLetter, 0);
-        }
-        catch (Exception e)
-        {
-            return false;
+            try
+            {
+                var letter1 = content.AllIndexOf(content[i].ToString(), StringComparison.Ordinal);
+                foreach (var currentLetter in letter1)
+                {
+                    indexes.Add(currentLetter, 0);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }    
         }
 
         return true;
