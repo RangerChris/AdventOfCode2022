839a43933b1caec3fb13657a76f22fc0de4e6239
Kresten Elleby <elleby@kresten.dk>
2022-12-14 22:38:24 +0100

    Day 12 work in progress

---
+++ AdventOfCode/Day12/Day12.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Day12;
+
+public class Day12
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day12(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    private static string DataPath => Path.Combine(Directory.GetCurrentDirectory(), "Day12\\Input.txt");
+    private static string DataPathHint => Path.Combine(Directory.GetCurrentDirectory(), "Day12\\Input-hint.txt");
+
+    [Fact]
+    public async Task Day12Hint1()
+    {
+        var input = await File.ReadAllTextAsync(DataPathHint);
+        input = input.Replace(" ->", "");
+        var rockData = input.Split(Environment.NewLine);
+        var gameWorld = new World(rockData);
+ 
+        gameWorld.WorldMinX.Should().Be(494);
+        gameWorld.WorldMaxX.Should().Be(503);
+        gameWorld.WorldMinY.Should().Be(4);
+        gameWorld.WorldMaxY.Should().Be(9);
+        
+        gameWorld.DrawHorizontalLine(497, 2, 7);
+        
+        _testOutputHelper.WriteLine(gameWorld.DrawWorld());
+    }
+    
+    [Fact]
+    public async Task Day12Puzzle1()
+    {
+        var input = await File.ReadAllTextAsync(DataPath);
+        input = input.Replace(" ->", "");
+        var rockData = input.Split(Environment.NewLine);
+        var gameWorld = new World(rockData);
+ 
+        gameWorld.WorldMinX.Should().Be(432);
+        gameWorld.WorldMaxX.Should().Be(518);
+        gameWorld.WorldMinY.Should().Be(13);
+        gameWorld.WorldMaxY.Should().Be(156);
+        
+        _testOutputHelper.WriteLine(gameWorld.DrawWorld());
+    }
+}
---
+++ AdventOfCode/Day12/World.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Day12;
+
+public class World
+{
+    public int WorldMinX;
+    public int WorldMaxX;
+    public int WorldMinY;
+    public int WorldMaxY;
+    private readonly char[,] _world;
+
+    private List<string> RockData { get; }
+    
+    public World(string[] rockData)
+    {
+        RockData = rockData.ToList();
+        CalculateWorldMinMax(rockData);
+        _world = new char[10, 10];
+    }
+
+    private void CalculateWorldMinMax(IEnumerable<string> rockData)
+    {
+        var allData = "";
+
+        foreach (var rock in rockData)
+        {
+            allData += rock + " ";
+        }
+
+        var dataList = allData.Trim().Split(" ").ToList();
+        var dataX = new List<int>();
+        var dataY = new List<int>();
+
+        foreach (var currentData in dataList)
+        {
+            var xy = currentData.Split(",");
+            dataX.Add(Convert.ToInt16(xy[0]));
+            dataY.Add(Convert.ToInt16(xy[1]));
+        }
+
+        WorldMinX = Convert.ToInt16(dataX.Min());
+        WorldMaxX = Convert.ToInt16(dataX.Max());
+        WorldMinY = Convert.ToInt16(dataY.Min());
+        WorldMaxY = Convert.ToInt16(dataY.Max());
+    }
+
+    public string DrawWorld()
+    {
+        var result = new StringBuilder();
+
+        for (var y = 0; y < _world.GetLength(1); y++)
+        {
+            for (var x = 0; x < _world.GetLength(0); x++)
+            {
+                if (_world[x, y] > 0)
+                {
+                    result.Append('#');
+                }
+                else
+                {
+                    result.Append('.');
+                }
+            }
+
+            result.Append(Environment.NewLine);
+        }
+
+        return result.ToString();
+    }
+
+    public int ToDrawXCoordinate(int inputCoordinate)
+    {
+        var result = inputCoordinate - WorldMinX;
+        return result;
+    }
+
+    public void DrawHorizontalLine(int x, int y, int length)
+    {
+        var xStart = ToDrawXCoordinate(x);
+        var maxX = xStart + length;
+        
+        for (int xx = xStart; xx < maxX; xx++)
+        {
+            _world[xx, y] = '#';
+        }
+    }
+    
+    public void DrawVerticalLine(int xStart, int xEnd)
+    {
+        
+    }
+}
